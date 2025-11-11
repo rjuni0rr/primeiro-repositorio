1a3e1aa4760c6effb92ab2492e2756081b6bf5fc
rjuni0rr <rjjuniorgamer@gmail.com>
2025-11-11 17:13:43 -0300

    migrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    // testar a conexão com a base de dados
+    try {
+        DB::connection()->getPdo();
+        echo "Conexão com o banco de dados OK!";
+    } catch (Exception $e) {
+        echo "Não foi possivel conectar com o banco de dados! Erro: " . $e->getMessage();
+    }
+
 });
